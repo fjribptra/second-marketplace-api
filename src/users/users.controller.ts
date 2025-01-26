0f1ba6e793c77169cfe8d5199bb320fe12568991
fjribptra <bagjaputrafajri@gmail.com>
2025-01-26 10:38:55 +0700

    feat: add interceptor and parsing data

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,9 +1,12 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Put } from '@nestjs/common';
+import { Body, ClassSerializerInterceptor, Controller, Delete, Get, Param, Patch, Post, Put, UseInterceptors } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dtos/update-user.dto';
+import { Serialize, SerializeInterceptor } from 'src/interceptors/serialize.interceptor';
+import { UserDto } from './dtos/user.dto';
 
 @Controller('users')
+@Serialize(UserDto)
 export class UsersController {
     constructor(private usersService: UsersService) {}
     @Get() 
